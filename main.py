c432561a82929d43f044d4887d59ae3ef9260317
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-22 16:37:11 +0100

    thing

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def main():
     ).to('cuda')
 
     # Initialize trainer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    task_loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001).to('cuda')
+    task_loss_fn = nn.MSELoss().to('cuda')
     trainer = MoETrainer(
         model=model,
         optimizer=optimizer,
