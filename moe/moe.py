492ba724624a40d5d2b61fa48ce6e1952a96949c
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-23 20:02:55 +0100

    thing

--- moe/moe.py
+++ moe/moe.py
@@ -66,7 +66,7 @@ class MixtureOfExperts(nn.Module):
         # Shape: [batch_size, num_experts, output_dim]
 
         projection_matrix = self.net(x).view(batch_size, expert_outputs.shape[2], expert_outputs.shape[2])
-        projection_matrix = torch.randn(batch_size, expert_outputs.shape[2], expert_outputs.shape[2])
+        projection_matrix = torch.randn(batch_size, expert_outputs.shape[2], expert_outputs.shape[2]).to('cuda')
         expert_outputs = batch_project_to_unique_subspaces(
             expert_outputs,
             projection_matrix
