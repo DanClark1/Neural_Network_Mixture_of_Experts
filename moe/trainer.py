438d746212e73f75bf9edc20ccf96e4780ec0328
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-26 13:58:42 +0100

    he

--- moe/trainer.py
+++ moe/trainer.py
@@ -49,19 +49,26 @@ class MoETrainer:
         lambda_losses = []
         with torch.no_grad():
             Z_full = []
+            e_outputs = [[] for _ in range(self.model.num_experts)]
             for x, y in val_loader:
                 x = x.to('cuda')
                 y = y.to('cuda')
                 outputs, cosine_loss, lambda_loss, *expert_outputs = self.model(x, record=record)
                 cosine_losses.append(cosine_loss)
                 lambda_losses.append(lambda_loss)
+                
                 if record:
                     e_outputs = expert_outputs[0].permute(1, 2, 0) # (dim, experts, batch) -> (experts, batch, dim)
                     for i in range(e_outputs.shape[0]):
-                        print(f'expert {i} variance:', torch.linalg.vector_norm(e_outputs[i].var(dim=0), dim=-1).cpu().numpy())
+                        e_outputs[i].append(torch.linalg.vector_norm(e_outputs[i].var(dim=0), dim=-1).cpu().numpy())
                 loss = self.task_loss_fn(outputs, y)
                 total_loss += loss.item()
                 num_batches += 1
+
+            for e in range(e_outputs):
+                e_outputs[e] = torch.stack(e_outputs[e], dim=0).mean(dim=0)
+                print(f"Expert {e} output variance: {e_outputs[e]}")            
+
 
         avg_cosine_loss = sum(cosine_losses) / len(cosine_losses)
         avg_lambda_loss = sum(lambda_losses) / len(lambda_losses)
